76aa73097493f2fa659cdd540e62174037bcffa4
Posquit0.BJ <posquit0.bj@gmail.com>
2017-09-23 18:25:07 +0900

    Add test cases for storages

--- test/storages/LocalStorage.test.js
+++ test/storages/LocalStorage.test.js
@@ -1,9 +1,14 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+const { Readable } = require('stream');
 const LocalStorage = require('../../storages/LocalStorage');
 
 
 describe('LocalStorage', () => {
+  const storage = new LocalStorage();
+
   describe('constructor(options)', () => {
     it('should create an instance of LocalStorage', () => {
       expect(new LocalStorage()).toBeInstanceOf(LocalStorage);
@@ -11,11 +16,36 @@ describe('LocalStorage', () => {
   });
 
   describe('download(filePath, options)', () => {
+    it('should create an instance of ReadableStream', async () => {
+      const src = await storage.download('non-exist.jpg');
+      expect(src).toBeInstanceOf(Readable);
+      src.on('error', () => {});
+    });
+    it('should trigger ENOENT error event when try to download non-exist file', async () => {
+      const src = await storage.download('non-exist.jpg');
+      expect(src).toBeInstanceOf(Readable);
+      src.on('error', err => {
+        expect(err.message).toMatch('ENOENT: no such file or directory');
+      });
+    });
   });
 
   describe('upload(src, filePath, options)', () => {
+    it('should return `undefined` when upload is finished', async () => {
+      const filename = 'package.json';
+      const srcPath = path.join(process.cwd(), filename);
+      const src = fs.createReadStream(srcPath);
+      expect(storage.upload(src, filename)).resolves.toBe();
+    });
   });
 
   describe('remove(filePath)', () => {
+    it('should reject with ENOENT error when try to remove non-exist file', async () => {
+      try {
+        await storage.remove('non-exist.jpg');
+      } catch (err) {
+        expect(err.message).toMatch('ENOENT: no such file or directory');
+      }
+    });
   });
 });
--- test/storages/S3Storage.test.js
+++ test/storages/S3Storage.test.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const { PassThrough } = require('stream');
 const S3Storage = require('../../storages/S3Storage');
 
 
 describe('S3Storage', () => {
+  const storage = new S3Storage({ bucket: 'test' });
+
   describe('constructor(options)', () => {
     it('should create an instance of S3Storage', () => {
       expect(new S3Storage()).toBeInstanceOf(S3Storage);
@@ -11,6 +14,10 @@ describe('S3Storage', () => {
   });
 
   describe('download(filePath, options)', () => {
+    it('should create an instance of PassThrough', async () => {
+      const src = await storage.download('non-exist.jpg');
+      expect(src).toBeInstanceOf(PassThrough);
+    });
   });
 
   describe('upload(src, filePath, options)', () => {
